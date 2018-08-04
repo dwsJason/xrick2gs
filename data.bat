27e73a4dc2c5d4827f7c39344b9437e29a8fdf74
dwsJason <JGAndersen@gmail.com>
2018-08-04 19:03:24 -0400

    data.bat:  annotate with comments

--- data.bat
+++ data.bat
@@ -18,11 +18,24 @@ if not exist "bmp" (
 	mkdir bmp
 )
 
+rem
+rem convert all the pngs to bmp
+rem
 png2bmp -D bmp data\pics\*.png
+rem
+rem for now just convert the splash screen, to GS format
+rem
 b2s bmp\splash.bmp
+rem
+rem Compress the title page
+rem
 lz4 -c2 bmp\splash.SHR#C10000 data\splash.lz4
-cd data
-iix mkobj splash_lz4 splash.lz4 splash.a
-cd ..
+rem
+rem Convert the title page into an object file
+rem
+iix mkobj splash_lz4 data:splash.lz4 data:splash.a
+rem
+rem Create Static Linked Binary data library
+rem
 iix makelib -P data.lib +data\splash.a
 
