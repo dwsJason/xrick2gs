bd18a221e38cfd63573447d6018c8a65e9c9fb7e
dwsJason <JGAndersen@gmail.com>
2018-08-05 16:04:24 -0400

    add img_splash to the data build

--- data.bat
+++ data.bat
@@ -26,16 +26,20 @@ rem
 rem for now just convert the splash screen, to GS format
 rem
 b2s bmp\splash.bmp
+b2s bmp\img_splash16.bmp
 rem
 rem Compress the title page
 rem
 lz4 -c2 bmp\splash.SHR#C10000 data\splash.lz4
+lz4 -c2 bmp\img_splash16.SHR#C10000 data\img_splash16.lz4
 rem
 rem Convert the title page into an object file
 rem
 iix mkobj splash_lz4 data:splash.lz4 data:splash.a
+iix mkobj img_splash_lz4 data:img_splash16.lz4 data:img_splash.a
 rem
 rem Create Static Linked Binary data library
 rem
 iix makelib -P data.lib +data\splash.a
+iix makelib -P data.lib +data\img_splash.a
 
