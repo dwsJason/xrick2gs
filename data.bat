8f96c222dbd8f2a5c40ec481c88b5a453f031b75
dwsJason <JGAndersen@gmail.com>
2018-11-23 16:02:30 -0500

    data.bat:  update to include the NTP songs into the executable

--- data.bat
+++ data.bat
@@ -23,6 +23,15 @@ iix mkobj img_splash_lz4 data:img_splash16.lz4 data:img_splash.a screendata
 iix mkobj pic_haf_lz4 data:pic_haf.lz4 data:pic_haf.a screendata
 iix mkobj tiles_lz4 data:tiles.lz4 data:tiles.a tilesdata
 rem
+rem NinjaTrackerPlus Songs to object files
+rem
+iix mkobj egypt_ntp audio:songs:egypt.ntp data:egypt.a ntpdata
+iix mkobj mbase_ntp audio:songs:mbase.ntp data:mbase.a ntpdata
+iix mkobj rick1_ntp audio:songs:rick1.ntp data:rick1.a ntpdata2
+iix mkobj rick1victory_ntp audio:songs:rick1victory.ntp data:rick1victory.a ntpdata2
+iix mkobj samerica_ntp audio:songs:samerica.ntp data:samerica.a ntpdata
+iix mkobj schwarz_ntp audio:songs:schwarz.ntp data:schwarz.a ntpdata
+rem
 rem Packed Mr.Sprites into object files
 rem
 iix mkobj xrickspr_00 data:sprites:xrick00.lz4 data:xrick0.a sprdata0
@@ -40,4 +49,17 @@ iix makelib -P data.lib +data\xrick0.a
 iix makelib -P data.lib +data\xrick1.a
 iix makelib -P data.lib +data\xrick2.a
 iix makelib -P data.lib +data\xrick3.a
+iix makelib -P data.lib +data:egypt.a
+iix makelib -P data.lib +data:mbase.a
+iix makelib -P data.lib +data:rick1.a
+iix makelib -P data.lib +data:rick1victory.a
+iix makelib -P data.lib +data:samerica.a
+iix makelib -P data.lib +data:schwarz.a
+rem
+rem NinjaTrackerPlus Library
+rem
+cd asm\merlin
+call make.bat
+cd ..\..
+iix makelib -P data.lib +asm\merlin\ntpplayer.a
 
